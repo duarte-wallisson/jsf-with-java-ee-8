11c583ff1378eafd9ef4386eca6bf574959e17ea
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-06-06 16:47:15 -0300

    feat: class 07 - working with collections

--- src/main/java/br/com/duarte/jsf/beans/student/StudentRegisterBean.java
+++ src/main/java/br/com/duarte/jsf/beans/student/StudentRegisterBean.java
@@ -6,10 +6,25 @@ import lombok.Data;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.*;
+
+import static java.util.Arrays.asList;
 
 @Named
 @Data
 public class StudentRegisterBean implements Serializable {
     @Inject
     private Student student;
+
+    private String[] greetingsArray = {"Hello", ", ", "World", "!"};
+    private List<String> greetingsList = asList("Hi", " There", "!");
+    private Set<String> greetingsSet = new HashSet<>(asList("Bye", ", ", "Bye!"));
+    private Map<String, String> greetingsMap = new HashMap<>();
+
+    {
+        greetingsMap.put("greeting", "Hi!");
+        greetingsMap.put("welcome", "Welcome!");
+        greetingsMap.put("farewell", "Goodbye!");
+    }
+
 }
